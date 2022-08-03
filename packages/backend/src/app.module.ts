f8bb47e66fc15bcf2e2937493479085b8bcd5cc6
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-04 01:39:50 +0900

    feat: 생성한 Module file 의존성 주입 및 정의된 Entity file DB 생성

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 
 import { DatabaseModule } from "@common/database.module";
+import { CategoriesModule } from "@categories/categories.module";
+import { MenusModule } from "@menus/menus.module";
+import { OptionsModule } from "@options/options.module";
+import { OptionDetailsModule } from "@optionDetails/optionDetails.module";
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: ".env" }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ".env" }),
+    DatabaseModule,
+    CategoriesModule,
+    MenusModule,
+    OptionsModule,
+    OptionDetailsModule,
+  ],
   controllers: [],
   providers: [],
 })

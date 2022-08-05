12fbae0f2b14d12363cf357baac75e20d1cfe7f4
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-05 12:43:50 +0900

    feat: CustomBaseEntity 적용 및 class-validator를 통한 type 적용
    - #4

--- packages/backend/src/menus/entities/menu.entity.ts
+++ packages/backend/src/menus/entities/menu.entity.ts
@@ -1,44 +1,41 @@
 import { Category } from "@categories/entities/category.entity";
 import { Option } from "@options/entities/option.entity";
+import { CustomBaseEntity } from "@src/core/CustomBaseEntity";
+import { IsArray, IsInt, IsObject, IsString } from "class-validator";
 import {
-  BaseEntity,
   Column,
-  CreateDateColumn,
+  DeleteDateColumn,
   Entity,
   JoinTable,
   ManyToMany,
   ManyToOne,
-  PrimaryGeneratedColumn,
   Repository,
   Unique,
-  UpdateDateColumn,
 } from "typeorm";
 
 @Entity({ name: "menu_table" })
 @Unique(["name"])
-export class Menu extends BaseEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
-
+export class Menu extends CustomBaseEntity {
   @Column({ type: "varchar", length: 30 })
-  name: string;
+  @IsString()
+  name!: string;
 
   @Column({ type: "text" })
-  imgUrl: string;
+  @IsString()
+  imgUrl!: string;
 
   @Column({ type: "int" })
-  price: number;
+  @IsInt()
+  price!: number;
 
-  @Column({ type: "int" })
-  sellCount: number;
+  @Column({ type: "int", default: 0 })
+  sellCount!: number;
 
-  @CreateDateColumn({ name: "createdAt" })
-  createdAt: Date;
-
-  @UpdateDateColumn({ name: "updatedAt" })
-  updatedAt: Date;
+  @DeleteDateColumn({ name: "deletedAt" })
+  deletedAt: Date;
 
   @ManyToOne(() => Category, (category) => category.menus)
+  @IsObject()
   category: Category;
 
   @ManyToMany(() => Option, (option) => option.menus)
@@ -53,7 +50,8 @@ export class Menu extends BaseEntity {
       referencedColumnName: "id",
     },
   })
-  options: Option[];
+  @IsArray()
+  options!: Option[];
 }
 
 export type MenuRepository = Repository<Menu>;

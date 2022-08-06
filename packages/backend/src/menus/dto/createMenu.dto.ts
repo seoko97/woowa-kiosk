d8b2d9d60646abbd95d87a9b04b9073573c9b654
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-06 14:45:54 +0900

    feat: DTO category -> categoryId 변경
    - #4

--- packages/backend/src/menus/dto/createMenu.dto.ts
+++ packages/backend/src/menus/dto/createMenu.dto.ts
@@ -1,9 +1,4 @@
 import { Menu } from "@menus/entities/menu.entity";
 import { PickType } from "@nestjs/mapped-types";
 
-export class CreateMenuDto extends PickType(Menu, [
-  "name",
-  "imgUrl",
-  "price",
-  "category",
-] as const) {}
+export class CreateMenuDto extends PickType(Menu, ["name", "imgUrl", "price", "categoryId"]) {}

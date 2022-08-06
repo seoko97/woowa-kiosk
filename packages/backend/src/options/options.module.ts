b51c329099e72c502f3f3632512810e1db2ae36f
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-06 14:49:57 +0900

    feat: Option 내부에서 service 사용을 위한 Module 주입
    - #4

--- packages/backend/src/options/options.module.ts
+++ packages/backend/src/options/options.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { MenusModule } from "@menus/menus.module";
+import { OptionDetailsModule } from "@optionDetails/optionDetails.module";
 import { OptionsService } from "./options.service";
 import { OptionsController } from "./options.controller";
 import { Option } from "./entities/option.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Option])],
+  imports: [TypeOrmModule.forFeature([Option]), MenusModule, OptionDetailsModule],
   controllers: [OptionsController],
   providers: [OptionsService],
 })

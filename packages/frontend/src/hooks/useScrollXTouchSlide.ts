f5c5c43fcc62920eabf35fa339dd06adb3833de3
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-08 01:01:22 +0900

    feat: useScrollXTouchSlide custom hook 생성 - mobile touch event 구현 - mousedown시 이벤트 시작 - mousemove시 좌우 스크롤 - mouseup시 이벤트 종료 - close #8

---
+++ packages/frontend/src/hooks/useScrollXTouchSlide.ts
@@ -0,0 +1,66 @@
+import { RefObject, useCallback, useEffect, useState } from "react";
+
+type IReturnType = [number, boolean];
+type IScrollXSlide = (ref: RefObject<HTMLDivElement>) => IReturnType;
+
+const useScrollXTouchSlide: IScrollXSlide = (ref) => {
+  const [isDrag, setIsDrag] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [dragStart, setDragStart] = useState(0);
+
+  const onDragStart = useCallback((e: MouseEvent) => {
+    e.preventDefault();
+    if (!ref.current) return;
+    const date = new Date();
+
+    setIsDrag(true);
+    setDragStart(date.getTime());
+    setStartX(e.pageX + ref.current.scrollLeft);
+  }, []);
+
+  const onDragEnd = useCallback(() => {
+    setIsDrag(false);
+  }, []);
+
+  const onDragMove = useCallback(
+    (e: MouseEvent) => {
+      if (!isDrag || !ref.current) return;
+      const { scrollWidth, clientWidth, scrollLeft } = ref.current;
+
+      ref.current.scrollLeft = startX - e.pageX;
+
+      if (scrollLeft === 0) {
+        setStartX(e.pageX);
+      } else if (scrollWidth <= clientWidth + scrollLeft) {
+        setStartX(e.pageX + scrollLeft);
+      }
+    },
+    [isDrag],
+  );
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    ref.current.addEventListener("mousedown", onDragStart);
+    ref.current.addEventListener("mouseleave", onDragEnd);
+    ref.current.addEventListener("mouseup", onDragEnd);
+    if (isDrag) {
+      ref.current.addEventListener("mousemove", onDragMove);
+    }
+
+    return () => {
+      if (!ref.current) return;
+
+      ref.current.removeEventListener("mousedown", onDragStart);
+      ref.current.removeEventListener("mouseup", onDragEnd);
+      ref.current.removeEventListener("mouseleave", onDragEnd);
+      if (isDrag) {
+        ref.current.removeEventListener("mousemove", onDragMove);
+      }
+    };
+  }, [isDrag]);
+
+  return [dragStart, isDrag];
+};
+
+export default useScrollXTouchSlide;

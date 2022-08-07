dd1f0e345b2bb21cf1900c82683db7a0bf5348dd
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-08 01:10:41 +0900

    feat: Header Component 생성 - touch slide 적용 - NavItem Click Menu API 요청 및 해당 Item 중앙 정렬 - drag 시작 시간을 계산하여 이벤트 실행 시정 결정

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,12 +1,53 @@
+import React, { useCallback, useEffect, useState } from "react";
 import { ThemeProvider } from "@emotion/react";
-import { theme } from "./theme";
-import GlobalStyle from "./theme/GlobalStyles";
+
+import GlobalStyle from "src/theme/GlobalStyles";
+import { theme } from "src/theme";
+
+import Header from "src/components/UI/organisms/Header";
+import AppLayout from "src/components/UI/template/AppLayout";
+
+import { requestGetCategories } from "src/apis/category";
+import { requestGetMenusByCategoryId } from "src/apis/menu";
+
+import { ICategory, ICategoryRes } from "src/types/category";
+import { IMenusRes } from "src/types/menu";
 
 function App() {
+  const [selected, setSelected] = useState("");
+  const [menus, setMenus] = useState<IMenusRes[]>([]);
+  const [categories, setCategory] = useState<ICategoryRes[]>([]);
+
+  const getCategories = useCallback(async () => {
+    const data = await requestGetCategories();
+
+    setCategory(data);
+  }, []);
+
+  const getMenus = useCallback(async (category: ICategory) => {
+    const data = await requestGetMenusByCategoryId(category.id);
+
+    setSelected(category.name);
+    setMenus(data);
+  }, []);
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  useEffect(() => {
+    if (menus.length || !categories.length) return;
+
+    getMenus(categories[0]);
+  }, [categories]);
+
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <div className="App">asd</div>
+      <GlobalStyle theme={theme} />
+      <AppLayout>
+        <Header selected={selected} categories={categories} getMenus={getMenus} />
+        <div>다른거</div>
+      </AppLayout>
     </ThemeProvider>
   );
 }

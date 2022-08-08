e2db247656f05f12b4d8bddae7e11e01db992beb
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-09 01:00:34 +0900

    feat: category type 변경

--- packages/frontend/src/types/menu.ts
+++ packages/frontend/src/types/menu.ts
@@ -2,16 +2,27 @@ import { ICategory } from "./category";
 import { CoreDataDto, CoreRes } from "./core";
 import { IOption } from "./option";
 
+interface ISaleByDate extends CoreDataDto {
+  date: string;
+  count: number;
+  menuName: string;
+}
+
 interface IMenu {
   name: string;
   imgUrl: string;
   price: number;
+  categoryName: string;
   category: ICategory;
-  sellCount: string;
   options: IOption[];
+  saleByDate: ISaleByDate;
 }
 
-type IMenuRes = Omit<IMenu & CoreDataDto, "sellCount">;
+interface IKeyInMenus {
+  [key: string]: IMenusRes[];
+}
+
+type IMenuRes = Omit<IMenu & CoreDataDto, "sellCount" | "categoryName">;
 type IMenusRes = Omit<IMenu & CoreDataDto, "options" | "category">;
 
 interface IRequestGetMenus extends CoreRes {
@@ -22,4 +33,4 @@ interface IRequestGetMenu extends CoreRes {
   menu: IMenuRes;
 }
 
-export type { IMenu, IMenuRes, IRequestGetMenu, IRequestGetMenus, IMenusRes };
+export type { IMenu, IMenuRes, IRequestGetMenu, IRequestGetMenus, IMenusRes, IKeyInMenus };

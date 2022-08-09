3d101f50af4879293a5300ff795bf38ea2b824a1
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-09 23:56:48 +0900

    feat: Iamge Atom Component 생성 - 반응형 이미지 컴포넌트 생성 - #13

---
+++ packages/frontend/src/components/UI/atoms/Image/index.tsx
@@ -0,0 +1,29 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+type Props = React.ImgHTMLAttributes<HTMLImageElement>;
+
+const Image = ({ width = "100%", height = "70%", ...props }: Props) => {
+  return (
+    <StyledImage width={width} height={height}>
+      <img {...props} />
+    </StyledImage>
+  );
+};
+
+const StyledImage = styled.div<Pick<Props, "width" | "height">>`
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  position: relative;
+
+  display: flex;
+  justify-content: center;
+
+  & img {
+    height: 100%;
+    position: absolute;
+    object-fit: cover;
+  }
+`;
+
+export default Image;

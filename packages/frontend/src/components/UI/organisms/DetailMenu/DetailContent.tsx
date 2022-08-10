517f813020e58af0f9b7d2b0e47890bb0d5a4839
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-10 20:18:00 +0900

    feat: MenuModal Form 분리 - state에 따른 애니메이션 적용 - #13

---
+++ packages/frontend/src/components/UI/organisms/DetailMenu/DetailContent.tsx
@@ -0,0 +1,56 @@
+import styled from "@emotion/styled";
+import useModal from "src/hooks/useModal";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const DetailContent = ({ children }: Props) => {
+  const [isShowOptions, , , handleShowOptions] = useModal();
+
+  return (
+    <>
+      <DetailContentHeader onClick={handleShowOptions} isShowOptions={isShowOptions}>
+        {isShowOptions ? "닫기" : "열기"}
+      </DetailContentHeader>
+      <Container isShowOptions={isShowOptions}>{children}</Container>
+    </>
+  );
+};
+
+const Container = styled.div<{ isShowOptions: boolean }>`
+  width: 100%;
+  top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+
+  flex-wrap: nowrap;
+  overflow: auto;
+  background-color: ${({ theme }) => theme.WHITE};
+
+  flex: ${({ isShowOptions }) => (isShowOptions ? 2 : 0)};
+  transition: flex 0.5s;
+
+  & > form {
+    transition: opacity 0.5s;
+    opacity: ${({ isShowOptions }) => (isShowOptions ? 1 : 0)};
+  }
+`;
+
+const DetailContentHeader = styled.div<{ isShowOptions: boolean }>`
+  width: 100%;
+  transition: padding 0.5s;
+  padding: ${({ isShowOptions }) => (isShowOptions ? "1rem" : "3rem")};
+  cursor: pointer;
+
+  border-radius: 10px 10px 0 0;
+  box-shadow: 0px -10px 20px rgba(0, 0, 0, 0.1);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default DetailContent;
---
+++ packages/frontend/src/components/UI/organisms/DetailMenu/index.tsx
@@ -0,0 +1,67 @@
+import { useCallback } from "react";
+import styled from "@emotion/styled";
+import { IMenuRes } from "src/types/menu";
+import DetailMenuInfo from "src/components/UI/molecules/DetailMenuInfo";
+import DetailMenuOption from "src/components/UI/molecules/DetailMenuOption";
+import DetailMenuFooter from "src/components/UI/molecules/DetailMenuFooter";
+
+import { useMenu } from "src/contexts/MenuContext";
+import { useCartAction } from "src/contexts/CartContext";
+
+import DetailContent from "./DetailContent";
+
+interface Props {
+  menu: IMenuRes;
+  onCloseModal: () => void;
+}
+
+const DetailMenu = ({ menu, onCloseModal }: Props) => {
+  const { options, name, imgUrl } = menu;
+  const menuState = useMenu();
+  const { addCartItem } = useCartAction();
+
+  const updateCart: React.MouseEventHandler<HTMLButtonElement> = useCallback(
+    (e) => {
+      e.preventDefault();
+      const { count } = menuState;
+
+      if (count === 0) return;
+
+      addCartItem({ ...menuState });
+    },
+    [menuState],
+  );
+
+  return (
+    <Container>
+      <div className="close" onClick={onCloseModal}>
+        닫기
+      </div>
+      <DetailMenuInfo menuInfo={{ name, imgUrl }} />
+      <DetailContent>
+        <DetailMenuOption options={options} />
+      </DetailContent>
+      <DetailMenuFooter onCloseModal={onCloseModal} updateCart={updateCart} />
+    </Container>
+  );
+};
+
+const Container = styled.section`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  & > .close {
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+    cursor: pointer;
+    font-size: 2rem;
+  }
+`;
+
+export default DetailMenu;

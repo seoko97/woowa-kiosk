5050d034c41f9c69f9736f83a48f4de04b464e7a
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-12 03:48:03 +0900

    chore: 배포관련 설정 변경

--- packages/frontend/src/apis/index.ts
+++ packages/frontend/src/apis/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3065/api" : process.env.HOST;
+  process.env.NODE_ENV === "development" ? "http://localhost:3065/api" : process.env.REACT_APP_HOST;
 
 export default axios;

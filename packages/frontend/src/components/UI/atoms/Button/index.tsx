6ab333704c127c1643658373ee9c0e6218c74147
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-11 13:01:15 +0900

    feat Button disabled 스타일 추가

--- packages/frontend/src/components/UI/atoms/Button/index.tsx
+++ packages/frontend/src/components/UI/atoms/Button/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo } from "react";
 import styled from "@emotion/styled";
 import { IColor, ISize } from "src/theme/types";
 
@@ -7,7 +7,7 @@ interface ButtonProps {
   bColor: IColor;
 }
 
-interface Props extends React.HTMLAttributes<HTMLButtonElement>, Partial<ButtonProps> {}
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement>, Partial<ButtonProps> {}
 
 const Button: React.FC<Props> = ({ size = "sm", bColor = "PRIMARY1", children, ...props }) => {
   return (
@@ -18,12 +18,20 @@ const Button: React.FC<Props> = ({ size = "sm", bColor = "PRIMARY1", children, .
 };
 
 const StyledButton = styled.button<ButtonProps>`
+  box-sizing: border-box;
   font-size: ${({ theme, size }) => theme.FONT_SIZE[size]};
   padding: ${({ theme, size }) => theme.BUTTON_SIZE[size]};
   background-color: ${({ theme, bColor }) => theme[bColor]};
   color: ${({ theme }) => theme.WHITE};
   border-radius: 10px;
   cursor: pointer;
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.WHITE};
+    color: ${({ theme, bColor }) => theme[bColor]};
+    border: 2px solid ${({ theme, bColor }) => theme[bColor]};
+    cursor: default;
+  }
 `;
 
-export default Button;
+export default memo(Button);
